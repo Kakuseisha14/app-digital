1848465d971177012e19f33528ffa4e885adb01d
Eduar Martinez <Kakuseisha14@gmail.com>
2023-03-10 10:35:21 -0400

    Cambios en el menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,93 +21,53 @@
                 </div>
             </li>
             <li class="#">
-                <a href="{{ url('/') }}"><i class="fa fa-th-large"></i> <span class="nav-label">Main view</span></a>
+                <a href="{{ url('/home') }}"><i class="fa fa-th-large"></i> <span class="nav-label">Panel principal</span></a>
             </li>
-           
-            <li >
-                <a href="#"><i class="fa fa-sitemap"></i> <span class="nav-label">Historia | Marcas Tem. </span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level collapse">
-                    <li>
-                        <a href="#" id="damian">Third Level <span class="fa arrow"></span></a>
-                        <ul class="nav nav-third-level">
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="#">Third Level Item</a>
-                            </li>
-
-                        </ul>
-                    </li>
-                    <li><a href="#">Second Level Item</a></li>
-                    <li>
-                        <a href="#">Second Level Item</a></li>
-                    <li>
-                        <a href="#">Second Level Item</a></li>
-                </ul>
-            </li>
+         
 
 
             <li>
-                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Servicio</span><span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label"> Gestión de servicios</span><span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level collapse">
-                    <li><a href="graph_flot.html">Flot Charts</a></li>
-                    <li><a href="graph_morris.html">Morris.js Charts</a></li>
-                    <li><a href="graph_rickshaw.html">Rickshaw Charts</a></li>
-                    <li><a href="graph_chartjs.html">Chart.js</a></li>
-                    <li><a href="graph_chartist.html">Chartist</a></li>
-                    <li><a href="c3.html">c3 charts</a></li>
-                    <li><a href="graph_peity.html">Peity Charts</a></li>
-                    <li><a href="graph_sparkline.html">Sparkline Charts</a></li>
+                    <li><a href="graph_flot.html">Cámara y comercio</a></li>
+                    <li><a href="graph_morris.html">Rut</a></li>
+                    <li><a href="graph_rickshaw.html">Facturación</a></li>
+                    <li><a href="graph_chartjs.html">Contratos</a></li>
+                    <li><a href="graph_chartist.html">Estados 
+                        financieros</a></li>
+                    <li><a href="c3.html">Obligaciones 
+                        tributarias</a></li>
+                    <li><a href="graph_peity.html"> Informes</a></li>
+                    <li><a href="graph_sparkline.html">Otros 
+                        tramites</a></li>
                 </ul>
             </li>
             <li>
-                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Economia</span><span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Servicios</span><span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level collapse">
-                    <li><a href="#">Administración</a></li>
-                    <li><a href="graph_flot.html">Ahorros</a></li>
-                    <li><a href="graph_flot.html">Trabajos</a></li>
-                    <li><a href="graph_flot.html">Objetivos</a></li>
+                    <li><a href="#">Cámara y comercio</a></li>
+                    <li><a href="graph_flot.html">Rut</a></li>
+                   
                     
                 </ul>
             </li>
          
             <li>
-                <a href="metrics.html"><i class="fa fa-pie-chart"></i> <span class="nav-label">Eventos</span>  </a>
+                <a href="metrics.html"><i class="fa fa-user-o"></i> <span class="nav-label">Usuarios</span>  </a>
+            </li>
+
+            <li>
+                <a href="metrics.html"><i class="fa fa-cube"></i> <span class="nav-label">Tablas Maestras</span>  </a>
             </li>
      
           
-         
-       
-      
-            <li>
-                <a href="#"><i class="fa fa-flask"></i> <span class="nav-label">Investigación</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level collapse">
-                    <li><a href="#">Armamento Bélico</a></li>
-                    <li><a href="#">Armamento Bélico2</a></li>
-                    <li><a href="icons.html">Geopolítica</a></li>
-                    <li><a href="draggable_panels.html">Tecnología informática</a></li> <li><a href="resizeable_panels.html">Resizeable Panels</a></li>
-              
-                </ul>
-            </li>
 
             <li>
-                <a href="grid_options.html"><i class="fa fa-laptop"></i> <span class="nav-label">Proyectos TI</span></a>
+                <a href="grid_options.html"><i class="fa fa-laptop"></i> <span class="nav-label">Asistencia técnica IT</span></a>
             </li>
            
            
-            <li>
-                <a href="#"><i class="fa fa-picture-o"></i> <span class="nav-label">Lugares | Sitios</span><span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level collapse">
-                    <li><a href="basic_gallery.html">Lightbox Gallery</a></li>
-                    <li><a href="slick_carousel.html">Slick Carousel</a></li>
-                    <li><a href="carousel.html">Bootstrap Carousel</a></li>
-
-                </ul>
-            </li>
+           
          
 
 
